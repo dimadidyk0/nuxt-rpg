ca8cb5a140a53bcfd615ad0e06712d10762970b9
dimadidyk0 <dimadidyk0@gmail.com>
2026-06-14 08:17:26 +0900

    feat: add template of inventar

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
+  <div class="app">
     Game over<br>
     You Win!
+
+    <footer class="footer">
+      <Inventar />
+    </footer>
   </div>
 </template>
+
+<style scoped>
+  .app {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .footer {
+    margin-top: auto;
+    display: flex;
+    padding: 24px;
+    border-top: 2px solid var(--color-indigo);
+    background: var(--color-dark-purple);
+  }
+</style>
---
+++ app/components/Inventar/Index.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+    const inventory = [
+        {
+            id: 1,
+            item: {
+                id: 1,
+                emoji: '🛡️',
+                quantity: 2,
+            }
+        },
+        {
+            id: 2,
+            item: {
+                id: 2,
+                emoji: '🧪',
+                quantity: 12,
+            }
+        },
+        { id: 3, item: null },
+        { id: 4, item: null },
+        { id: 5, item: null },
+    ];
+</script>
+
+<template>
+    <div class="list">
+        <div v-for="{id, item} in inventory" :key="id"  class="item" >
+            <div v-if="item">
+                {{ item.emoji }}
+                <span class="quantity">{{ item.quantity }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .list {
+        display: flex;
+        gap: 4px;
+        margin: auto;
+    }
+
+    .item {
+        height: 60px;
+        width: 60px;
+        border: 1px solid var(--color-cyan-teal);
+        background: var(--color-indigo);
+        border-radius: 4px;
+        font-size: 36px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+
+        &:not(:empty) {
+            background-color: var(--color-dark-teal);
+            border-color: var(--color-aqua);;
+
+            &:active {
+                background-color: var(--color-teal-green);
+            }
+            @media (hover: hover) {
+                &:hover {
+                    background-color: var(--color-teal-green);
+                }
+            }
+        }
+    }
+
+    .quantity {
+        position: absolute;
+        bottom: 2px;
+        right: 4px;
+        font-size: 14px;
+        font-weight: bold;
+        color: var(--color-sand-gold);
+    }
+</style>
