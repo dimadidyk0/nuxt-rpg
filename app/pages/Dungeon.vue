8c8a10e7f87897b4bfc83ee4d26fb8e1d57e6d22
dimadidyk0 <dimadidyk0@gmail.com>
2026-06-18 19:35:24 +0900

    feat: add dungeon template

---
+++ app/pages/Dungeon.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="root">
+    <h2 class="title">
+      Level 1
+    </h2>
+    <div class="content">
+      Enemies...
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  .root {
+    flex-grow: 1;
+    padding-top: 24px;
+  }
+
+  .title {
+    font-size: 24px;
+    text-align: center;
+    margin-top: 0;
+    margin-bottom: 24px;
+  }
+
+  .content {
+
+  }
+</style>
