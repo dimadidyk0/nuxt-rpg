1702c21a41183a4aeb89d594c313dcb26accf37f
dimadidyk0 <dimadidyk0@gmail.com>
2026-06-21 21:39:12 +0900

    chore: add assets and setup dungeon

--- app/pages/Dungeon.vue
+++ app/pages/Dungeon.vue
@@ -4,7 +4,22 @@
       Level 1
     </h2>
     <div class="content">
-      Enemies...
+      <NuxtImg 
+        :src="'/assets/images/enemies/skeleton.gif'"
+        class="img"
+        width="384" 
+        height="384" 
+        alt="item.alt"
+        loading="lazy"
+      />
+      <NuxtImg 
+        :src="'/assets/images/dungeon.png'"
+        class="bg"
+        width="384" 
+        height="384" 
+        alt="item.alt"
+        loading="lazy"
+      />
     </div>
   </main>
 </template>
@@ -22,7 +37,30 @@
     margin-bottom: 24px;
   }
 
-  .content {
+  .img {
+    position: relative;
+    left: -10px;
+  }
 
+  .img {
+    animation: movement 1s infinite steps(1);
+  }
+
+  @keyframes movement {
+    0%   { bottom: 0; }
+    50%  { bottom: 4px; }
+    100%  { bottom: 0; }
+  }
+
+  .content {
+    border: 8px solid var(--color-burgundy);
+    position: relative;
+  }
+
+  .bg {
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 0;
   }
 </style>
