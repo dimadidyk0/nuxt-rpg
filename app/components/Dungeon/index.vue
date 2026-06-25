c766a202485e98259db3f02649653ba28e11a4de
dimadidyk0 <dimadidyk0@gmail.com>
2026-06-25 16:30:00 +0900

    feat: add dungeon component and reorganize things

---
+++ app/components/Dungeon/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content">
+    <slot></slot>
+
+    <NuxtImg
+      :src="'/assets/images/dungeon.png'"
+      class="bg"
+      width="384"
+      height="384"
+      alt="item.alt"
+      loading="lazy"
+    />
+  </div>
+</template>
+
+<style scoped>
+.content {
+  border: 8px solid var(--color-burgundy);
+  position: relative;
+}
+
+.bg {
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+}
+</style>
--- app/pages/Dungeon.vue
+++ app/pages/Dungeon.vue
@@ -1,21 +1,13 @@
 <template>
   <main class="root">
     <h2 class="title">Level 1</h2>
-    <div class="content">
+    <Dungeon>
       <Monster
         name="Skeleton"
         image="/assets/images/enemies/skeleton.gif"
         :health="{ current: 14, max: 30 }"
       />
-      <NuxtImg
-        :src="'/assets/images/dungeon.png'"
-        class="bg"
-        width="384"
-        height="384"
-        alt="item.alt"
-        loading="lazy"
-      />
-    </div>
+    </Dungeon>
   </main>
 </template>
 
@@ -30,17 +22,5 @@
   text-align: center;
   margin-top: 0;
   margin-bottom: 24px;
-}
-
-.content {
-  border: 8px solid var(--color-burgundy);
-  position: relative;
-}
-
-.bg {
-  position: absolute;
-  z-index: -1;
-  top: 0;
-  left: 0;
 }
 </style>
